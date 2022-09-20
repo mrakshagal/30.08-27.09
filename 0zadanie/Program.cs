21f48b1ee65044ae3348b75dc4dfd352b502786b
Nikita Kotov <nikitakot233@icloud.com>
2022-09-20 21:22:41 +0200

    changes

---
+++ 0zadanie/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход
+// 1.принимает число и 2.выдаёт его квадрат (число
+// умноженное на само себя).
+// 3.wynik
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+Console.Clear();
+Console.Write( "Type integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num * num;
+Console.WriteLine($"Square of number {num} = {square}");
+
+
