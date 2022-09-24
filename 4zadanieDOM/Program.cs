e866af8100383195c6c81b55779783cd4794c34d
Nikita Kotov <nikitakot233@icloud.com>
2022-09-24 18:26:33 +0200

    TASK 4 DONE

---
+++ 4zadanieDOM/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
+
+Console.Clear();
+Console.Write("Type three integer numbers: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    if (num1 > num3)
+    {
+        Console.WriteLine($"The biggest number is {num1}");
+    }
+    else if (num1 < num3)
+    {
+        Console.WriteLine($"The biggest number is {num3}");
+    }
+}
+else if (num2 > num1)
+{
+    if (num2 > num3)
+    {
+        Console.WriteLine($"The biggest number is {num2}");
+    }
+    else if (num2 < num3)
+    {
+        Console.WriteLine($"The biggest number is {num3}");
+    }
+}
+else 
+{
+    Console.WriteLine("Wrong numbers.Try again");
+}
