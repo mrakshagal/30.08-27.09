70c5ef578f73394dab738870a83ea88fdc8d41f9
Nikita Kotov <nikitakot233@icloud.com>
2022-09-26 01:30:36 +0200

    task 9 done

---
+++ 9zadanie/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая выводит случайное число из отрезка от 10 до 99 и показывает наибольшую цифру числа
+
+int number = new Random().Next(10, 100);
+int firstdigit = number / 10;
+int seconddigit = number % 10;
+if (firstdigit > seconddigit)
+{
+    Console.WriteLine($"The biggest digit of {number} is {firstdigit}");
+}
+else
+{
+    Console.WriteLine($"The biggest digit of {number} is {seconddigit}");
+}
