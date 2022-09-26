45be963298acebd911f8e8c324792293af8391ef
Nikita Kotov <nikitakot233@icloud.com>
2022-09-26 02:01:05 +0200

    11zadanie done

---
+++ 11zadanie/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int num = new Random().Next(100, 1000);
+// int firstdigit = number / 100
+// int thirddigit = number % 10
+int MaxDigit(int num1)
+{
+    int firstdigit= num1 / 100 * 10;
+    int seconddigit= num1 % 10;
+    return firstdigit + seconddigit;
+}
+int result = MaxDigit(num);
+Console.WriteLine($"{num}->{result}");
+
+
+
