d35ef82f8adaf65917cffc006b1c94fbf2667ce8
Nikita Kotov <nikitakot233@icloud.com>
2022-09-27 09:40:16 +0200

    task 12 done

---
+++ 12zadanie/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Type integer number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Type one more integer number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3= num1%num2;
+if (num1>num2)
+{
+    if (num3==0)
+    {
+        Console.WriteLine($"{num1} is multiple of the number {num2}");
+    }
+    else 
+    {
+        Console.WriteLine($"{num1} is not multiple of the number {num2} and the rest is {num3}");
+    }
+}
+else 
+{
+    Console.WriteLine("Try again");
+}
+
