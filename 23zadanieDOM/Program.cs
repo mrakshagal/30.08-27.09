707f1a963d2c21d5b2761480d350e2a87ca8d5e5
Nikita Kotov <nikitakot233@icloud.com>
2022-09-27 15:21:22 +0200

    task23 done

---
+++ 23zadanieDOM/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Таблица кубов натуральных чисел");
+Console.WriteLine("-------------");
+Console.Write("Введите положительное целое число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+void Cube(int digit1)
+{
+    if (digit1 >= 1)
+    {
+        for (int i = 1; i <= digit1; i++)
+        {
+            Console.WriteLine($"{i} --> {i * i * i,4}");
+        }
+    }
+    else Console.WriteLine($"Введено некорректное число");
+}
+Console.WriteLine();
+Cube(digit);
