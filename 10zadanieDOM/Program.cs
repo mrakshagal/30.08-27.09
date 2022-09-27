79749b654d852bc7ebe519a775114fb8f91a4ab7
Nikita Kotov <nikitakot233@icloud.com>
2022-09-27 12:51:47 +0200

    task 10 done

---
+++ 10zadanieDOM/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.Write("Type three-digit number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int twodigits = num1 % 100;
+int seconddigit = twodigits /10;
+if (num1 > 99 && num1 < 1000)
+{
+    Console.WriteLine($"{seconddigit}");
+}
+else 
+{
+    Console.WriteLine($"Try again");
+}
