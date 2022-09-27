f7c83222a71fe558dee31ee023a6b496f2929fe0
Nikita Kotov <nikitakot233@icloud.com>
2022-09-27 10:43:34 +0200

    task 16 done

---
+++ 16zadanie/Program.cs
@@ -0,0 +1,18 @@
+﻿//Два числа. является ли одно число квадратом другого 
+Console.Clear();
+Console.Write("Type integer number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Type one more integer number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1/num2==num2)
+{
+    Console.WriteLine($"{num1} is the square of {num2}");
+}
+else if (num2/num1==num1)
+{
+    Console.WriteLine($"{num2} is the square of {num1}");
+}
+else 
+{
+    Console.WriteLine("Try again");
+}
