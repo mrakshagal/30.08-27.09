a4fa45532d32a63ef8c64dd95f0b36f4e660c352
Nikita Kotov <nikitakot233@icloud.com>
2022-09-27 10:22:05 +0200

    task 14 done

---
+++ 14zadanie/Program.cs
@@ -0,0 +1,20 @@
+﻿// Принимает число и проверяет, кратно ли оно одновременно 7 и 23
+
+Console.Clear();
+Console.Write("Type integer number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1%7 ==0)
+{
+    if(num1%23==0)
+    {
+        Console.WriteLine ($"{num1} is multiple of 7 and 23");
+    }
+    else
+    {
+        Console.WriteLine($"{num1} is not multiple of 7 and 23");
+    }
+}
+else
+{
+    Console.WriteLine($"{num1} is not multiple of 7 and 23");
+}
