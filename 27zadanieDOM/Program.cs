be2d8d4d9072df3d24611d0f5456ff747710282b
Nikita Kotov <nikitakot233@icloud.com>
2022-09-30 14:54:57 +0200

    task 27 done

---
+++ 27zadanieDOM/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите любое целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int accum = 0;
+int count = 0;
+if (num < 0) num = -num;
+int Countingdigits(int num1, int accum1, int i)
+{
+    while (num1 != 0)
+    {
+        accum1 = accum1 + num1 % 10;
+        num1 = num1 / 10;
+        i++;
+    }
+    return accum1;
+}
+int result = Countingdigits(num, accum, count);
+Console.WriteLine("-------------------------");
+Console.WriteLine($"Сумма цифр в введенном числе составит {result}");
