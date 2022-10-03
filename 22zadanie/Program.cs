192c66987f657c1bc6466467f0c98a85a1dc1395
Nikita Kotov <nikitakot233@icloud.com>
2022-10-03 19:32:03 +0200

    task 22 done

---
+++ 22zadanie/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход натуральное число и показывает квадраты чисел на расстоянии от 1 до этого числа 
+
+Console.Clear();
+Console.Write("Type integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+void squares (int num1, int count1)
+{
+    while(count1 <= num1)
+    {
+        Console.WriteLine($"{count1}-> {count1*count1}, ");
+        count1++;
+    }
+}
+squares(num, count);
