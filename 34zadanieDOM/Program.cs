8dc24b2ec14db260d077b24b33df80fda3780e36
Nikita Kotov <nikitakot233@icloud.com>
2022-10-04 16:03:08 +0200

    task 34 solved

---
+++ 34zadanieDOM/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Clear();
+Console.WriteLine("Подсчет четных чисел в массиве");
+Console.WriteLine("------------------------------");
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+int CountingEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+int[] arr = CreateArrayRndInt(10, 100, 999);
+PrintArray(arr);
+Console.WriteLine();
+Console.WriteLine("------------------------------");
+Console.WriteLine($"Количество четных чисел в массиве: {CountingEvenNumbers(arr)}");
+Console.WriteLine();
