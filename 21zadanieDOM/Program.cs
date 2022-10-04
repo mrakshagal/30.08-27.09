3cc33d066af03c7e34dcc42cb9ad92b40bb59bb0
Nikita Kotov <nikitakot233@icloud.com>
2022-10-04 15:51:59 +0200

    task 21 solved

--- 21zadanieDOM/Program.cs
+++ 21zadanieDOM/Program.cs
@@ -1,23 +1,26 @@
-﻿Console.Clear();
-Console.WriteLine("Нахождение расстояние между двумя точками в трехмерном пространстве");
-Console.WriteLine("Введите координаты точки A: ");
-Console.Write("x:");
-double ax = Convert.ToDouble(Console.ReadLine());
-Console.Write("y:");
-double ay = Convert.ToDouble(Console.ReadLine());
-Console.Write("z:");
-double az = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите координаты точки B: ");
-Console.Write("x:");
-double bx = Convert.ToDouble(Console.ReadLine());
-Console.Write("y:");
-double by = Convert.ToDouble(Console.ReadLine());
-Console.Write("z:");
-double bz = Convert.ToDouble(Console.ReadLine());
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-double Result(double ax1, double ay1, double az1, double bx1, double by1, double bz1)
-{
-    return Math.Sqrt((ax1 - bx1) * (ax1 - bx1) + (ay1 - by1) * (ay1 - by1) + (az1 - bz1) * (az1 - bz1));
-}
-double result = Result(ax, ay, az, bx, by, bz);
-Console.WriteLine($"Расстояние между точками составит: {Math.Round(result, 2, MidpointRounding.ToZero)}");
+// A (3,6,8); B (2,1,-7) -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+Console.WriteLine("Koordinaty pierwoj tochki.");
+Console.Write("X: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Koordinaty wtoroj tochki.");
+Console.Write("X: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double dif1 =Math.Pow(x2-x1, 2);
+double dif2 =Math.Pow(y2-y1, 2);
+double dif3 =Math.Pow(z2-z1, 2);
+double result = Math.Sqrt(dif1+dif2+dif3);
+Console.WriteLine($"A ({x1},{y1},{z1}); B ({x2},{y2},{z2}) -> {result} ");
