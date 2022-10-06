c4443779ca3d320a7e6c1541280038f0ec6b5a6a
Nikita Kotov <nikitakot233@icloud.com>
2022-10-06 21:45:13 +0200

    task 24 done

---
+++ 24zadanie/Program.cs
@@ -0,0 +1,17 @@
+﻿// Принимает на вход число и выдает сумму чисел от 1 до этого числа 
+
+Console.Clear();
+Console.Write("Type integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int Summary (int num1)
+{
+    int sum = default;
+    for (int i = 1; i <= num1; i++)
+    {
+// sum= sum+i
+sum += i;
+    }
+    return sum;
+}
+int summar = Summary(num);
+Console.WriteLine($"Summary of numbers from 1 to {num} is {summar}");
