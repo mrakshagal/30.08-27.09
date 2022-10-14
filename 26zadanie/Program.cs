327c50dae4f8d63c7b09e5a556fedd271b8cea83
Nikita Kotov <nikitakot233@icloud.com>
2022-10-14 19:15:51 +0200

    task 26 done

---
+++ 26zadanie/Program.cs
@@ -0,0 +1,24 @@
+﻿// Take a number and write the number of digits in it
+Console.Clear();
+Console.Write("Type the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Count(int num)
+{
+int i;
+if (num/10==0)
+{
+    i=1;
+}
+else
+{
+    for (i=1; i<=num; i++)
+{
+num= num/10;
+}
+}
+return i;
+}
+
+int digits = Count(number);
+Console.WriteLine($"Number {number} contains {digits} digits");
