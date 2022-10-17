10307645f812082c2116a8dbfe774c7fb8210f56
Nikita Kotov <nikitakot233@icloud.com>
2022-10-17 16:32:23 +0200

    task 28 done

---
+++ 28zadanie/Program.cs
@@ -0,0 +1,16 @@
+﻿// Take the number and write multiplication of numbers from 1 to N
+Console.Clear();
+Console.WriteLine("Type integer number:");
+int num = Convert.ToInt32(Console.ReadLine());
+int Mult (int num1)
+{
+    int multi = 1;
+    for (int i = 1; i <= num1; i++)
+    {
+
+multi = multi*i;
+    }
+    return multi;
+}
+int multik = Mult(num);
+Console.WriteLine($"Multiplication of numbers from 1 to {num} is {multik}");
