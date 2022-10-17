5cbc691aef2e7cbd94fed54220311957312bf092
Nikita Kotov <nikitakot233@icloud.com>
2022-10-17 16:32:01 +0200

    30 zadanie done

---
+++ 30zadanie/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int[] arr = {5,5,5,5,5,5,5,5};
+void PrintArray(int[] array)
+{
+    int count= array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i]= new Random().Next(0,2);
+        Console.Write($"{array[i]}");
+    }
+Console.WriteLine();
+}
+PrintArray(arr);
