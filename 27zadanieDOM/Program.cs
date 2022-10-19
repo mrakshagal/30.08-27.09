28a478a1b26e561a09588f71dad93608d992b8b9
Nikita Kotov <nikitakot233@icloud.com>
2022-10-19 15:10:08 +0200

    task 27 done

--- 27zadanieDOM/Program.cs
+++ 27zadanieDOM/Program.cs
@@ -1,19 +1,20 @@
-﻿Console.Clear();
-Console.Write("Введите любое целое число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+﻿// The summary of digits of the number
+
+Console.Clear();
+Console.WriteLine("Type the number:");
+int a = Convert.ToInt32(Console.ReadLine());
 int accum = 0;
 int count = 0;
-if (num < 0) num = -num;
-int Countingdigits(int num1, int accum1, int i)
+if (a < 0) a=-a;
+int countsummaryofdigits (int num1, int accum1, int i)
 {
-    while (num1 != 0)
+    while (num1 !=0)
     {
-        accum1 = accum1 + num1 % 10;
-        num1 = num1 / 10;
+        accum1= accum1+ num1%10;
+        num1= num1/10;
         i++;
     }
     return accum1;
 }
-int result = Countingdigits(num, accum, count);
-Console.WriteLine("-------------------------");
-Console.WriteLine($"Сумма цифр в введенном числе составит {result}");
+int result = countsummaryofdigits ( a,  accum,  count);
+Console.WriteLine($"The summary of digits in number {a} is {result}");
