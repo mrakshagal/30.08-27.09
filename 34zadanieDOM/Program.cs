4b5b6f17a203dc675acfb96d292e5bffb49dfbc5
Nikita Kotov <nikitakot233@icloud.com>
2022-11-14 10:08:51 +0100

    task 34 done

--- 34zadanieDOM/Program.cs
+++ 34zadanieDOM/Program.cs
@@ -1,39 +1,38 @@
-﻿Console.Clear();
-Console.WriteLine("Подсчет четных чисел в массиве");
-Console.WriteLine("------------------------------");
+﻿// задайте массив, рандмно заполненный положительными трехзначными числами. Показать количество четных элементов в массиве
 
-int[] CreateArrayRndInt(int size, int min, int max)
+Console.Clear();
+int[] CreateArrayRndInt (int size, int min, int max)
 {
     int[] array = new int[size];
     Random rnd = new Random();
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i< size; i ++)
     {
-        array[i] = rnd.Next(min, max + 1);
+        array[i]= rnd.Next(min, max+1);
     }
-    return array;
+return array; 
 }
-void PrintArray(int[] array)
+void PrintArray (int[] array)
 {
-    Console.Write("[");
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
-        else Console.Write($"{array[i]}");
-    }
-    Console.Write("]");
+Console.Write("[");
+for (int i = 0; i< array.Length; i++)
+{
+    if (i< array.Length-1) Console.Write($"{array[i]}, ");
+    else Console.Write($"{array[i]}");
 }
-int CountingEvenNumbers(int[] array)
+Console.Write("]");
+}
+int CountEvenNumbers (int[] array)
 {
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
+    int count = default;
+    for (int i = 0; i< array.Length; i++)
     {
-        if (array[i] % 2 == 0) count++;
+        if (array[i]%2==0) count++;
     }
     return count;
 }
-int[] arr = CreateArrayRndInt(10, 100, 999);
+
+int[] arr = CreateArrayRndInt(6, 100, 999);
 PrintArray(arr);
+int result = CountEvenNumbers(arr);
 Console.WriteLine();
-Console.WriteLine("------------------------------");
-Console.WriteLine($"Количество четных чисел в массиве: {CountingEvenNumbers(arr)}");
-Console.WriteLine();
+Console.Write($"There is {result} even numbers");
