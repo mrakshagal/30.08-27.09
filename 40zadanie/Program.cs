0294a038b53af380b8cbf30753a7b8a80184a674
Nikita Kotov <nikitakot233@icloud.com>
2022-11-28 18:50:42 +0100

    task 40 done

---
+++ 40zadanie/Program.cs
@@ -0,0 +1,10 @@
+﻿// Prinimaet tri chisla i proveriaet mozhet li exist trójkąt so storonami that dliny 
+Console.Clear();
+Console.Write("Type the number of the first side: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Type the nimber of the second side: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Type the number of the third side: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a+b>c && a+c>b && c+b>a) Console.Write("This triangle can exist!");
+else Console.Write("Try another numbers!");
