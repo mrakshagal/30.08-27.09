af170a291f660dd65aad970fed00603820b64fe6
Nikita Kotov <nikitakot233@icloud.com>
2022-11-28 19:50:54 +0100

    task 42 done

---
+++ 42zadanie/Program.cs
@@ -0,0 +1,18 @@
+﻿// Desyatichnoe chislo w dwoichnoe
+Console.Clear();
+Console.WriteLine("Type the number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int ConvertNumber(int num)
+{
+    int result = default;
+    int count = 1;
+    while (num !=0)
+    {
+        result = (num%2)*count +result;
+        num/=2;
+        count*=10;
+    }
+    return result; 
+}
+Console.Write(ConvertNumber(a));
