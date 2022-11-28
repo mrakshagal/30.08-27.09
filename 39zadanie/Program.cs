34dba68764d10f95cc9929b21b3eb0362287b750
Nikita Kotov <nikitakot233@icloud.com>
2022-11-28 18:17:24 +0100

    task 39 done

---
+++ 39zadanie/Program.cs
@@ -0,0 +1,41 @@
+﻿// Programma, kotoraya perevernet odnomernyi massiv 
+Console.Clear();
+int[] CreateArrayRndInt (int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size ; i ++)
+    {
+        array[i] = rnd.Next(min, max+1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i<array.Length-1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+void ReverseArray (int[] array)
+{
+    int size = array.Length;
+    int index1 = 0;
+    int index2 = size-1;
+    while (index1<index2)
+    {
+        int obj = array[index1];
+        array[index1]= array[index2];
+        array[index2]= obj;
+        index1 ++;
+        index2 --;
+    }
+}
+int[] arr= CreateArrayRndInt(5, 1, 10);
+PrintArray(arr);
+Console.WriteLine();
+ReverseArray(arr);
+PrintArray(arr);
