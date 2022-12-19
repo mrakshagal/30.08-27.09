b5fd17cf94de838bef916b4c996f8309812f46c2
Nikita Kotov <nikitakot233@icloud.com>
2022-12-19 19:03:02 +0100

    task 46 done

---
+++ 46zadanie/Program.cs
@@ -0,0 +1,31 @@
+﻿// massiv razmerom m x n, zapolnennyi randomnymi chislami m= 3, n=4
+
+Console.Clear();
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMartix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}, ");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("]");
+    }
+}
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMartix(array2d);
