8d7b4086797fe7cdffa48724153e8cfc1b0d0269
Nikita Kotov <nikitakot233@icloud.com>
2022-12-25 16:40:49 +0100

    task 51 done

---
+++ 51zadanie/Program.cs
@@ -0,0 +1,47 @@
+﻿//Massiv i najti summu po diagonali
+
+Console.Clear();
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMartix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}, ");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("|");
+    }
+}
+int SumMidleMatrix(int[,] matrix)
+{
+    int result = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j) result += matrix[i, j];
+        }
+    }
+    return result;
+}
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMartix(array2d);
+int sumResult = SumMidleMatrix(array2d);
+
+
+Console.WriteLine($"{sumResult}");
