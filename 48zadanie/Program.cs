3afeb3c34bea0a4919b581e20004e4502af8f6ee
Nikita Kotov <nikitakot233@icloud.com>
2022-12-25 15:03:42 +0100

    task 48 done

---
+++ 48zadanie/Program.cs
@@ -0,0 +1,32 @@
+﻿// massiv m x n. Amn= m+n- element massiva
+
+Console.Clear();
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i+ j;
+        
+        }
+    }
+    return matrix;
+}
+void PrintMartix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} ");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("]");
+    }
+}
+int [,] array = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMartix(array);
