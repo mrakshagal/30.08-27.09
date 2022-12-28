d53c075570321f61cc6bc063fee5033b7f7a70f8
Nikita Kotov <nikitakot233@icloud.com>
2022-12-28 18:54:38 +0100

    task 64 done

---
+++ 64zadanieDOM/Program.cs
@@ -0,0 +1,32 @@
+﻿int NaturalNumber(int a, int b)
+{
+    if (a == b)
+        return a;
+    else
+        Console.Write($"{NaturalNumber(a, b + 1)}, ");
+    return b;
+}
+
+
+Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Вывод всех натуральных чисел в промежутке от N до 1");
+Console.WriteLine("###################################################");
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.Write("Введите положительное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+if (n < 1)
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("Введено некорректное число");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("*****************************");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine(NaturalNumber(n, m));
+}
